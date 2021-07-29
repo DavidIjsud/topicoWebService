7b87668c645613b472b925660ff5ceb722af03bd
David Garcia <david.garcia@oktana.com>
2021-07-29 17:26:19 -0400

    Main modificado

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', '/public'));
   app.useGlobalPipes( new ValidationPipe({
         transform : true,
         whitelist : true
