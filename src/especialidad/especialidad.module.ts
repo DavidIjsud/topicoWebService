bf35ccd57975d759f9ae3c1ff1260d57520b1eba
David Garcia <david.garcia@oktana.com>
2021-08-06 23:07:29 -0400

    Se agrego a medico para que me traiga datos de su relacioes

--- src/especialidad/especialidad.module.ts
+++ src/especialidad/especialidad.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Especialidad } from 'src/entities/Especialidad';
 import { EspecialidadController } from './especialidad.controller';
 import { EspecialidadService } from './services/especialidad.service';
 
 @Module({
   controllers: [EspecialidadController],
-  providers: [EspecialidadService]
+  providers: [EspecialidadService],
+  imports : [ TypeOrmModule.forFeature([Especialidad]) ]
 })
 export class EspecialidadModule {}
